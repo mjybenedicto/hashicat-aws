2ff26a12423cfcf8b2b7cd7833faedb17a2d5ae1
Michael Benedicto <mjybenedicto@gmail.com>
2021-10-13 04:35:13 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/tf_workshop_mjyb/s3-bucket/aws"
+  version = "2.2.0"
+  # insert required variables here
   bucket_prefix = "michael-benedicto"
-
-  versioning = {
-    enabled = true
-  }
-
 }
